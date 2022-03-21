45111246f8cfa9b6c98c00c792e735a087787499
Space-18 <nicolasrodrigo1973@gmail.com>
2022-03-21 12:06:19 -0500

    Asp.Net Core || Aplicacion web basica(GUID)

---
+++ 6-MovieApp/Controllers/AppController.cs
@@ -0,0 +1,67 @@
+﻿using _6_MovieApp.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _6_MovieApp.Controllers
+{
+    public class AppController : Controller
+    {
+        public static List<MovieAppViewModel> _movies =  new List<MovieAppViewModel>();
+
+        public IActionResult Index()
+        {
+            return View(_movies);
+        }
+
+        //[HttpGet("Edit")]
+        /* Modificacion mia para que genere el identificador
+         */
+        public IActionResult AddOrEdit(Guid id)
+        {
+            var movie = _movies.FirstOrDefault(x => x.Identi == id);
+            return View(movie);
+        }
+
+        /* en lugar de poner x.Id se reemplaza por x.Identi
+         * para recoger el identificador generado automaticamente 
+         */
+        [HttpPost]
+        public IActionResult AddOrEdit(MovieAppViewModel model)
+        {
+            var movie = _movies.FirstOrDefault(x => x.Identi == model.Id);
+
+            if (ModelState.IsValid)
+            {
+                if (movie == null)
+                {  
+                    _movies.Add(model);
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    movie.Genero = model.Genero;
+                    movie.Nombre = model.Nombre;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(movie);
+        }
+
+        [HttpGet("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        /* modificacion para que funcione bien
+         */
+        public IActionResult Delete(Guid id)
+        {
+            _movies.Remove(_movies.FirstOrDefault(x => x.Identi == id));
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ 6-MovieApp/ViewModel/MovieAppViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _6_MovieApp.ViewModel
+{
+    public class MovieAppViewModel
+    {
+        /* esto lo agregue yo, en la clase del profesor no aparece esto,
+         * pero para poder generar un identificador necesite de esto 
+         * y de algunas modificaciones en el controller
+         */
+        public Guid Identi = Guid.NewGuid();
+
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Por favor Ingrese un nombre")]
+        [MaxLength(30, ErrorMessage = "Has excedido el maximo de caracteres")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Por favor Ingrese un genero")]
+        [MaxLength(30, ErrorMessage = "Has excedido el maximo de caracteres")]
+        public string Genero { get; set; }
+    }
+}
